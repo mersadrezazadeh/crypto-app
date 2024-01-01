c78d47fc701f5abbfcf79e05e60134d53a8b2541
mersad98 <mersadrezazadeh@gmail.com>
2024-01-01 10:10:58 +0330

    add size to button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -18,6 +18,8 @@ function Button({ size, disabled, onClick, children }: ButtonProps) {
       onClick={onClick}
       className={`bg-brand-600 text-brand-100 hover:bg-brand-700 rounded shadow-lg transition-all duration-300 disabled:opacity-50 disabled:pointer-events-none ${
         size === "small" ? sizes.small : ""
+      } ${size === "medium" ? sizes.medium : ""} ${
+        size === "large" ? sizes.large : ""
       }`}
     >
       {children}
