5bd8e2af25ff24172dda085f892d11354432e195
mersad98 <mersadrezazadeh@gmail.com>
2024-01-01 17:37:32 +0330

    clean up

--- components/coin-details/CoinDetails.tsx
+++ components/coin-details/CoinDetails.tsx
@@ -1,54 +1,8 @@
 import { getCoinDetails } from "@/services/apiCoins";
 import CoinOverview from "./CoinOverview";
+import CoinChart from "./CoinChart";
 import CoinMarket from "./CoinMarket";
 import CoinDescription from "./CoinDescription";
-import CoinChart from "./CoinChart";
-
-type Coin = {
-  name: string;
-  symbol: string;
-  image: {
-    small: string;
-  };
-  links: {
-    homepage: string[];
-  };
-  market_data: {
-    current_price: {
-      usd: number;
-      eur: number;
-      gbp: number;
-      jpy: number;
-    };
-    price_change_percentage_24h: number;
-    sparkline_7d: {
-      price: number[];
-    };
-    market_cap: {
-      usd: number;
-      eur: number;
-      gbp: number;
-      jpy: number;
-    };
-    fully_diluted_valuation: {
-      usd: number;
-      eur: number;
-      gbp: number;
-      jpy: number;
-    };
-    total_volume: {
-      usd: number;
-      eur: number;
-      gbp: number;
-      jpy: number;
-    };
-    circulating_supply: number;
-    max_supply: number;
-  };
-  description: {
-    en: string;
-  };
-};
 
 type CoinDetailsProps = {
   selectedCoin: string;
@@ -110,23 +64,3 @@ async function CoinDetails({ selectedCoin, currency }: CoinDetailsProps) {
 }
 
 export default CoinDetails;
-
-// name
-// symbol
-// small image
-// links
-// current_price
-// price_change_percentage_1h_in_currency
-// ath
-// atl
-// ath_date
-// atl_date
-// description
-// blockchain_site
-// total_volume
-// mcap_to_tvl_ratio
-// market_cap
-// market_cap_change_24h
-// fully_diluted_valuation
-// max_supply
-// circulating_supply
