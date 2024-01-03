78047548a101f14febf6a74583abdd7e242eb624
mersad98 <mersadrezazadeh@gmail.com>
2024-01-03 20:14:05 +0330

    add icon

--- components/ui/PriceChange.tsx
+++ components/ui/PriceChange.tsx
@@ -1,11 +1,16 @@
+import { FaCaretUp, FaCaretDown } from "react-icons/fa6";
+
 function PriceChange({ value }: { value: number }) {
   const isPositive = value >= 0;
 
   return (
-    <div>
-      <span className={`${isPositive ? "text-green-200" : "text-red-200"}`}>
-        {value?.toFixed(2)}%
-      </span>
+    <div
+      className={`flex items-center gap-0.5 ${
+        isPositive ? "text-green-200" : "text-red-200"
+      }`}
+    >
+      {isPositive ? <FaCaretUp /> : <FaCaretDown />}
+      <span>{Math.abs(+value?.toFixed(2))}%</span>
     </div>
   );
 }
